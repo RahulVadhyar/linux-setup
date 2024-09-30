03c55a7c09c7ecb0fc9bb909ce74fcf665105bc2
Rahul <vadhyarrahul@gmail.com>
2024-09-30 13:34:31 +0530

    fixes to script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Variables
-PACKAGE_FILE="packages.txt"
 DNF_CONF="dnf.conf"
 GIT_NAME="Rahul Vadhyar"
 GIT_EMAIL="vadhyarrahul@gmail.com"
@@ -22,16 +21,11 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 sudo dnf upgrade -y
 
-if [ -f "$PACKAGE_FILE" ]; then
-    echo "Installing packages from $PACKAGE_FILE..."
-    packages=$(tr ' ' < "$PACKAGE_FILE")
-    sudo dnf install -y $packages --allowerasing --skipbroken
-    sudo dnf groupinstall -y "Development Tools"
-    sudo dnf groupupdate multimedia
-else
-    echo "Package file $PACKAGE_FILE not found!"
-    exit 1
-fi
+echo "Installing packages.."
+sudo dnf install -y zsh git python3.11 python3-devel python3-pip bpytop nvtop gh cloc code kate cmake g++ gdb clang lldb whereami xlsclients neofetch ckb-next chromium flatpak git-lfs vlc ffmpeg kmod akmods mokutil openssl gcc kernel-headers kernel-devel util-linux vulkan vulkan-validation-layers tokodon blender kdenlive --allowerasing --skipbroken
+
+sudo dnf groupinstall -y "Development Tools"
+sudo dnf groupupdate -y multimedia
 #
 # echo "Update fstab configuration..."
 # sudo echo "UUID=EBBF-75C2                            /run/media/baby2           exfat   rw,user,exec,uid=1000,gid=1000,umask=000    0 0"  >> /etc/fstab
@@ -47,20 +41,19 @@ pip install torch torchvision torchaudio matplotlib pandas scipy scikit-learn nu
 
 echo "Making ZSH default shell..."
 chsh -s $(which zsh)
-zsh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh); git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions; git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting; git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting; git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete"
+curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
 
 # Check if .zshrc exists in the home directory
 
 ZSHRC="$HOME/.zshrc"
 
-if [ -f "$ZSHRC" ]; then
-    # Use sed to replace the plugins line
-    sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting fast-syntax-highlighting zsh-autocomplete)/' "$ZSHRC"
-    echo ".zshrc has been updated "
-else
-    echo ".zshrc not found in the home directory."
-    exit 1
-fi
+# Use sed to replace the plugins line
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting fast-syntax-highlighting zsh-autocomplete)/' "$ZSHRC"
+echo ".zshrc has been updated "
 
 
 echo "Secure boot key generation..."
